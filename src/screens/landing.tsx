98976033d81763038122cdd060426042486401c1
Luan rodrigues <rodrigues.luan20@gmail.com>
2022-12-30 10:54:03 -0300

    fix: landingpage style adjustment

--- src/screens/landing.tsx
+++ src/screens/landing.tsx
@@ -1,22 +1,22 @@
 import { Button, Image, Text, TouchableOpacity, View } from "react-native"
 import { useNavigation } from "@react-navigation/native"
+import styles from "./style"
 
 const Landing = () =>{
     const navigation = useNavigation()
 
     return( 
-        <View>
-            <View>
-                <Text>Controle Financeiro</Text>
-                <Text>Aplicativo com foco em auxiliar na sua organização financeira</Text>
-                <Text>De forma rápida e segura!</Text>
-                <Text >landing</Text>
+        <View style={styles.landingContainer}>
+            <View style={styles.headerContainer}>
+                <Text style={styles.textTitle}>Controle Financeiro</Text>
+                <Text style={styles.textDescription}>Aplicativo com foco em auxiliar na sua organização financeira</Text>
+                <Text style={styles.textSpan}>De forma rápida e segura!</Text>
                 <TouchableOpacity 
+                style={styles.accessButton}
                 onPress={() => navigation.navigate("Home")}>
-                    <Text>Entrar</Text>
+                    <Text style={styles.textAccessButton}>Entrar</Text>
                 </TouchableOpacity>
             </View>
-            {/* <Button onPress={() => navigation.navigate("Home")} title="Entrar"/> */}
             <Image source={require("../../assets/imglanding.png")}/>
         </View>
     )
