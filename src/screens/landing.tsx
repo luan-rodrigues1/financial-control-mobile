1c6d4ccc0f4f8679c62563dcf15416246bf6437d
Luan rodrigues <rodrigues.luan20@gmail.com>
2022-12-30 09:58:26 -0300

    fix: landing screen editing

--- src/screens/landing.tsx
+++ src/screens/landing.tsx
@@ -1,13 +1,25 @@
-import { Button, Text, View } from "react-native"
+import { Button, Image, Text, TouchableOpacity, View } from "react-native"
 import { useNavigation } from "@react-navigation/native"
 
 const Landing = () =>{
     const navigation = useNavigation()
 
-    return <View>
-        <Text >landing</Text>
-        <Button onPress={() => navigation.navigate("Home")} title="Entrar"/>
-    </View>
+    return( 
+        <View>
+            <View>
+                <Text>Controle Financeiro</Text>
+                <Text>Aplicativo com foco em auxiliar na sua organização financeira</Text>
+                <Text>De forma rápida e segura!</Text>
+                <Text >landing</Text>
+                <TouchableOpacity 
+                onPress={() => navigation.navigate("Home")}>
+                    <Text>Entrar</Text>
+                </TouchableOpacity>
+            </View>
+            {/* <Button onPress={() => navigation.navigate("Home")} title="Entrar"/> */}
+            <Image source={require("../../assets/imglanding.png")}/>
+        </View>
+    )
 }
 
 export default Landing
