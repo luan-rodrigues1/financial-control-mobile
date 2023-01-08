151dfcbb9590d92c1e5a5f4a53412f7d70f2c2b0
Luan rodrigues <rodrigues.luan20@gmail.com>
2023-01-08 19:35:44 -0300

    feat: componente Card adionado e ajuste no componente CardList

---
+++ src/components/Card/style.ts
@@ -0,0 +1,53 @@
+import { StyleSheet} from "react-native";
+
+const styles = StyleSheet.create({
+    listContainer: {
+        alignItems: "center",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        borderWidth: 1,
+        borderColor: "#2C8CF4",
+        borderRadius: 20,
+        marginLeft: "7%",
+        marginRight: "7%",
+        height: 80,
+        paddingLeft: 15,
+        paddingRight: 20,
+        backgroundColor: "#ffffff",
+        marginBottom: 15,
+    },
+
+    cardBox: {
+        flexDirection: "row",
+        alignItems: "center",
+    },
+
+    textCard: {
+        fontSize: 15,
+        marginLeft: 10,
+        width: 200
+    },
+
+    textValue: {
+        fontSize: 17,
+        marginLeft: 10,
+        color: "#54C17F"
+    },
+
+    textValueExpense: {
+        fontSize: 17,
+        marginLeft: 10,
+        color: "#DD1E91"
+    },
+
+    trashButton: {
+        backgroundColor: "#2C8CF4",
+        width: 40,
+        height: 45,
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 10
+    }
+})
+
+export default styles
--- src/components/CardList/index.tsx
+++ src/components/CardList/index.tsx
@@ -1,16 +1,14 @@
-import { ReactNode, useContext, useState } from "react"
-import { FlatList, Image, Text, TouchableOpacity, View } from "react-native"
+import { useContext } from "react"
+import { FlatList, View } from "react-native"
 import { UserContext } from "../../contexts/UserContext"
 import NoTransactions from "../NoTransactions"
+import Card from "../Card"
 import styles from "./style"
 
 const CardList = () =>{
-    const {listTransaction, 
-        setDeleteModalVisibility, 
-        setdeleteId, 
+    const {listTransaction,
         usingFilter, 
-        listFiltred,
-        formattingCurrency
+        listFiltred
     } = useContext(UserContext)
 
    return (
@@ -18,28 +16,8 @@ const CardList = () =>{
             {!usingFilter && listTransaction[0] === undefined || usingFilter === true && listFiltred[0] === undefined ? <NoTransactions/> : 
             <FlatList
             data={!usingFilter ? listTransaction.slice().reverse() : listFiltred.slice().reverse()}
-            renderItem={({item}) =>{
-                    return (
-                    <View style={styles.listContainer}>
-                        <View style={styles.cardBox}>
-                            <Image source={item.type === "Despesa" ? require("../../../assets/arrowDown.png") : require("../../../assets/arrowGreen.png")}/>
-                            <View>
-                                <Text style={styles.textCard}>{item.description}</Text>
-                                <Text style={item.type === "Despesa" ? styles.textValueExpense : styles.textValue}>R$ {formattingCurrency(parseFloat(item.value)) as ReactNode}</Text>
-                            </View>
-                        </View>
-                        <TouchableOpacity
-                        style={styles.trashButton}
-                            onPress={()=> {
-                                setdeleteId(item.id),
-                                setDeleteModalVisibility(true)
-                            }}>
-                            <Image source={require("../../../assets/trash.png")}/>
-                        </TouchableOpacity>
-                    </View>
-                )
-            }}
-            >
+            renderItem={({item}) => <Card cardItem={item}/>
+            }>
             </FlatList>}
         </View>
     )
